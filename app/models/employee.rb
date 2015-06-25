74533155233452920f5f759a3c111dbb09a26df7
Reid Paape <wrpaape@gmail.com>
2015-06-25 17:57:46 -0500

    added counter caches for has many through associations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,20 @@ class Employee < ActiveRecord::Base
   has_many :courses, through: :participants, dependent: :destroy
   belongs_to :company, counter_cache: true
   belongs_to :location, counter_cache: true
+  after_create :increment_courses_counter_cache
+  after_destroy :decrement_courses_counter_cache
+
+  private
+
+  def increment_courses_counter_cache
+    self.courses.each do |course|
+      Course.increment_counter( 'courses_count', course.id )
+    end
+  end
+
+  def decrement_courses_counter_cache
+    self.courses.each do |course|
+      Course.decrement_counter( 'courses_count', course.id )
+    end
+  end
 end
