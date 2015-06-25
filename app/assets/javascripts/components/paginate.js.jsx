2e1bfac907547dcd8c516be3a1c9d3e49409b076
Reid Paape <wrpaape@gmail.com>
2015-06-25 12:07:47 -0500

    paginate component has basic functionality

--- app/assets/javascripts/components/paginate.js.jsx
+++ app/assets/javascripts/components/paginate.js.jsx
@@ -1,33 +1,34 @@
 /* globals React */
 'use strict';
 var Paginate = React.createClass({
-
-  // getInitialState: function () {
-  //     return {
-  //         page: 1;
-  //     };
-  // },
-
   render: function () {
+    var rows = [];
     for (var i = 0; i < this.props.data.length; i++) {
-      return (
-        <div class="row">
-          <div class="col-sm-1" ></div>
-          <div class="col-sm-11">
-            <h3>{ this.props.data[i].name }</h3>
-            <NavLink name='Show' url={ '/' + this.props.type + '/' + this.props.data[i].id } />
-            <NavLink name='Edit' url={ '/' + this.props.type + '/' + this.props.data[i].id + '/edit' } />
-            <NavLink name='Destroy' url={ '/' + this.props.type + '/' + this.props.data[i].id + '/delete' } />
-          </div>
+      rows.push(<DataRow elem={ this.props.data[i] } type={ this.props.type } />);
+    }
+    return (<div>{rows}</div>);
+  }
+});
+
+var DataRow = React.createClass({
+  render: function () {
+    return (
+      <div className="row">
+        <div className="col-sm-1"></div>
+        <div className="col-sm-11">
+          <h3>{ this.props.elem.name }</h3>
+          <NavLink name='Show' url={ '/' + this.props.type + '/' + this.props.elem.id } method='get' />
+          <NavLink name='Edit' url={ '/' + this.props.type + '/' + this.props.elem.id + '/edit' } method='get' />
+          <NavLink name='Destroy' url={ '/' + this.props.type + '/' + this.props.elem.id } method='delete' />
         </div>
-      );
-    };
+      </div>
+    );
   }
 });
 
 var NavLink = React.createClass({
   render: function () {
-    return (<a onClick={this.clicked} className='btn btn-default'>{this.props.name}</a>);
+    return (<a onClick={this.clicked} dataMethod={this.props.method} className='btn btn-default'>{this.props.name}</a>);
   },
   clicked: function () {
     window.location.href = this.props.url;
